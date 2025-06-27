afb233922531641c8fb6d0a9721a4b9ec974ac55
Hanmin Li <hanmin.li@kaust.edu.sa>
2025-06-27 13:50:09 +0300

    Add explanations

--- data/fineweb.py
+++ data/fineweb.py
@@ -20,11 +20,10 @@ import argparse
 import multiprocessing as mp
 import numpy as np
 import tiktoken
-# from huggingface_hub import snapshot_download
-from datasets import load_dataset
 from tqdm import tqdm
-import argparse
-import numpy as np
+from datasets import load_dataset
+
+
 def write_datafile(filename, toks):
     """ 
     Saves token data as a .bin file, for reading in C.
@@ -52,75 +51,112 @@ def write_datafile(filename, toks):
         f.write(toks_np.tobytes())
 # ------------------------------------------
 
-parser = argparse.ArgumentParser(description="FineWeb dataset preprocessing")
-parser.add_argument("-v", "--version", type=str, default="10B", help="Which version of fineweb to use 10B|100B")
-parser.add_argument("-s", "--shard_size", type=int, default=10**8, help="Size of each shard in tokens")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="FineWeb dataset preprocessing")
+    parser.add_argument("-v", "--version", type=str, default="10B", help="Which version of fineweb to use 10B|100B")
+    parser.add_argument("-s", "--shard_size", type=int, default=10**8, help="Size of each shard in tokens")
+    args = parser.parse_args()
+    ############################################################################################################
+    #   Explanation:
+    #       Shard:
+    #           A shard is just a chunk or segment of the dataset. Each .bin file is
+    #           a shard containing shard_size tokens (except possibly the last shard, which can be smaller).
+    #           Very large datasets (like FineWeb) don’t fit easily in a single file or RAM.
+    #           Splitting into shards makes data loading and training scalable and efficient.
+    ############################################################################################################
 
-# FineWeb has a few possible subsamples available
-assert args.version in ["10B", "100B"], "version must be one of 10B, 100B"
-if args.version == "10B":
-    local_dir = "fineweb10B"
-    remote_name = "sample-10BT"
-elif args.version == "100B":
-    local_dir = "fineweb100B"
-    remote_name = "sample-100BT"
+    # FineWeb has a few possible subsamples available
+    assert args.version in ["10B", "100B"], "version must be one of 10B, 100B"
+    if args.version == "10B":
+        local_dir = "fineweb10B"
+        remote_name = "sample-10BT"
+    elif args.version == "100B":
+        local_dir = "fineweb100B"
+        remote_name = "sample-100BT"
 
-# create the cache the local directory if it doesn't exist yet
-DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
-os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+    # create the cache the local directory if it doesn't exist yet
+    DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+    os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 
-# download the dataset
-fw = load_dataset("HuggingFaceFW/fineweb", name=remote_name, split="train")
+    # download the dataset
+    fw = load_dataset("HuggingFaceFW/fineweb", name=remote_name, split="train")
 
-# init the tokenizer
-enc = tiktoken.get_encoding("gpt2")
-eot = enc._special_tokens['<|endoftext|>'] # end of text token
-def tokenize(doc):
-    # tokenizes a single document and returns a numpy array of uint16 tokens
-    tokens = [eot] # the special <|endoftext|> token delimits all documents
-    tokens.extend(enc.encode_ordinary(doc["text"]))
-    tokens_np = np.array(tokens)
-    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
-    tokens_np_uint16 = tokens_np.astype(np.uint16)
-    return tokens_np_uint16
+    # init the tokenizer
+    enc = tiktoken.get_encoding("gpt2") 
+    eot = enc._special_tokens['<|endoftext|>'] # end of text token
+    def tokenize(doc):
+        # tokenizes a single document and returns a numpy array of uint16 tokens
+        tokens = [eot] # the special <|endoftext|> token delimits all documents
+        tokens.extend(enc.encode_ordinary(doc["text"]))
+        tokens_np = np.array(tokens)
+        assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
+        tokens_np_uint16 = tokens_np.astype(np.uint16)
+        return tokens_np_uint16
+    ############################################################################################################
+    #   Explanation:
+    #       tokens_np_uint16:
+    #           The downstream training code (e.g., nanoGPT) 
+    #           expects the tokens in binary .bin files as 16-bit unsigned integers for fast loading.
+    ############################################################################################################
+    
 
-# tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
-nprocs = max(1, os.cpu_count() - 2) # don't hog the entire system
-with mp.Pool(nprocs) as pool:
-    shard_index = 0
-    # preallocate buffer to hold current shard
-    all_tokens_np = np.empty((args.shard_size,), dtype=np.uint16)
-    token_count = 0
-    progress_bar = None
-    for tokens in pool.imap(tokenize, fw, chunksize=16):
+    # tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
+    nprocs = max(1, os.cpu_count() - 2) # don't hog the entire system
+    with mp.Pool(nprocs) as pool:
 
-        # is there enough space in the current shard for the new tokens?
-        if token_count + len(tokens) < args.shard_size:
-            # simply append tokens to current shard
-            all_tokens_np[token_count:token_count+len(tokens)] = tokens
-            token_count += len(tokens)
-            # update progress bar
-            if progress_bar is None:
-                progress_bar = tqdm(total=args.shard_size, unit="tokens", desc=f"Shard {shard_index}")
-            progress_bar.update(len(tokens))
-        else:
-            # write the current shard and start a new one
+        shard_index = 0
+        # preallocate buffer to hold current shard
+        all_tokens_np = np.empty((args.shard_size,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = None
+
+        for tokens in pool.imap(tokenize, fw, chunksize=16):
+            ############################################################################################################
+            #   Explanation:
+            #       imap:
+            #           iterative mapping, apply tokenize() to each element (doc) in the fw data object
+            #       fw:
+            #           the whole dataset, a sequence of documents (docs)
+            #       chunksize:
+            #           Each worker process gets a batch of 16 documents at a time
+            #           to tokenize (makes it more efficient, less overhead than one-at-a-time).
+            #
+            #       Each time through the loop, tokens is the result of running tokenize(doc) on a document from fw 
+            #       (done in parallel). We equentially receive tokenized outputs from all the worker processes 
+            #       as soon as they're ready, and process them one at a time in the main process. (Notice that 
+            #       the order of the docs are the same, if we are using pool.imap_unordered, then the order could 
+            #       be different.)
+            ############################################################################################################
+
+            # is there enough space in the current shard for the new tokens?
+            if token_count + len(tokens) < args.shard_size:
+                # simply append tokens to current shard
+                all_tokens_np[token_count : token_count + len(tokens)] = tokens
+                token_count += len(tokens)
+                # update progress bar
+                if progress_bar is None:
+                    progress_bar = tqdm(total=args.shard_size, unit="tokens", desc=f"Shard {shard_index}")
+                progress_bar.update(len(tokens))
+            else:
+                # write the current shard and start a new one
+                split = "val" if shard_index == 0 else "train"
+                filename = os.path.join(DATA_CACHE_DIR, f"fineweb_{split}_{shard_index:06d}.bin")
+
+                # split the document into whatever fits in this shard; the remainder goes to next one
+                remainder = args.shard_size - token_count
+                progress_bar.update(remainder)
+
+                all_tokens_np[token_count : token_count + remainder] = tokens[ : remainder]
+                write_datafile(filename, all_tokens_np)
+
+                shard_index += 1
+                progress_bar = None
+                # populate the next shard with the leftovers of the current doc
+                all_tokens_np[0:len(tokens) - remainder] = tokens[remainder : ]
+                token_count = len(tokens) - remainder
+
+        # write any remaining tokens in the buffer `all_tokens_np` as the last shard
+        if token_count != 0:
             split = "val" if shard_index == 0 else "train"
             filename = os.path.join(DATA_CACHE_DIR, f"fineweb_{split}_{shard_index:06d}.bin")
-            # split the document into whatever fits in this shard; the remainder goes to next one
-            remainder = args.shard_size - token_count
-            progress_bar.update(remainder)
-            all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
-            write_datafile(filename, all_tokens_np)
-            shard_index += 1
-            progress_bar = None
-            # populate the next shard with the leftovers of the current doc
-            all_tokens_np[0:len(tokens)-remainder] = tokens[remainder:]
-            token_count = len(tokens)-remainder
-
-    # write any remaining tokens as the last shard
-    if token_count != 0:
-        split = "val" if shard_index == 0 else "train"
-        filename = os.path.join(DATA_CACHE_DIR, f"fineweb_{split}_{shard_index:06d}.bin")
-        write_datafile(filename, all_tokens_np[:token_count])
+            write_datafile(filename, all_tokens_np[:token_count])
